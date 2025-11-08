fe6e84d7e097bf1bae9ac37ab7dbfacdbd741eda
Carasbugtrackr <christina34mea@gmail.com>
2025-11-08 16:45:11 -0600

    html

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
+// /src/main.jsx
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import CarasQASite from "./CarasQASite.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CarasQASite />
   </React.StrictMode>
 );
+
