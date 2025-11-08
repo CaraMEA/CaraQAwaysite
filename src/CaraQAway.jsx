791e9e65a1a73849d59cc81bef3ebc1d8267fd26
Carasbugtrackr <christina34mea@gmail.com>
2025-11-08 16:24:45 -0600

    images

--- src/CaraQAway.jsx
+++ src/CaraQAway.jsx
@@ -1,7 +1,7 @@
-{
+export default {
   id: "externship",
   title: "TripleTen Externship",
   content:
     "I completed my first automation project during a 7-week externship with TripleTen, collaborating with Runestone Academy. Real-world QA, real deadlines, real growth.",
   link: "https://www.canva.com/design/DAGqubVQdQw/uR7leU_PbFvh1IZdmlToUA/view",
-}
+};
