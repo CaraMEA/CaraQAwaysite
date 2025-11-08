e658aed1fe232f023f7e58133e8ae875b2f46d6f
Carasbugtrackr <christina34mea@gmail.com>
2025-11-08 15:35:42 -0600

    moved images

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CarasQASite from "./CarasQASite.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CarasQASite />
+  </React.StrictMode>
+);
