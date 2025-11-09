176b949abe2ba77d52cc1276bd2154bb3524fd46
Carasbugtrackr <christina34mea@gmail.com>
2025-11-08 20:47:54 -0600

    main.jsx change

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 // /src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import CarasQASite from "./CarasQASite.jsx";
+import CaraQAway from "./CaraQAway.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CarasQASite />
+    <CaraQAway />
   </React.StrictMode>
 );
 
